0b2ef301e1c654094c2837577c890e7e79baf021
Бридский Александр <bridskiy@JUPITER9.RU>
2022-02-20 15:56:27 +0300

    add new files exampl 008-011

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+int [] array = {1, 42, 32324, 443, 566, 675, 775, 86, 9};
+
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(max);
