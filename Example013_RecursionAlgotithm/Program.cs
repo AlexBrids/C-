0f748bbd395c41aa195b0c0e105d5a9246db214f
Бридский Александр <bridskiy@JUPITER9.RU>
2022-06-02 18:33:44 +0300

    get first lesson recursion

--- Example013_RecursionAlgotithm/Program.cs
+++ Example013_RecursionAlgotithm/Program.cs
@@ -21,7 +21,7 @@ for (int rows = 0; rows < 2; rows++)
 
 
 
-
+/*
 void PrintArray(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -51,11 +51,11 @@ PrintArray(matrix);
 FillArray(matrix);
 WriteLine();
 PrintArray(matrix);
+*/
 
 
 
-
-
+/*
 int[,] pic = new int[,]
  {
      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -82,4 +82,87 @@ int[,] pic = new int[,]
      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
  };
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            //Write($"{image[i, j]}");
+            if(image[i, j] == 0)
+            {
+                Write($" ");
+            }
+            else
+            {
+                Write($"+");
+            }
+        }
+        WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+*/
+
+/*
+double Factorial (int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+for (int i = 1; i < 40; i++)
+{
+    WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+//WriteLine(Factorial(5));
+
+
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+/*
+double Fibonacci(int n)
+{
+    if(n == 1 || n ==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+for (int i = 1; i < 50; i++)
+{
+    WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
+*/
+
 
